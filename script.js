da0c521a2d60cb26d92d82b93e1abcd1a4d725f0
Avery Yang <averyyang2674@gmail.com>
2022-06-09 16:26:39 -0500

    add limit to buttons

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ const circles = $('.circle');
 
 //counter for button
 let button = 1
+console.log(button);
 
 //condition
 function ready() {
     $('#next').on('click', () => {
         button++;
         console.log(button);
+        if (button >= circles.length) {
+            button = circles.length
+        }
     });
 }
