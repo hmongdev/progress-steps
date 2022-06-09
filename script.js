8ce3ecacb4609e64bb54886e8f84a5c6dac13bb2
Avery Yang <averyyang2674@gmail.com>
2022-06-09 15:38:41 -0500

    add ready function; next button adds count

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+$(document).ready(ready);
+
 //declaring variables by targeting id's
 const progress = $('#progress');
 const prev = $('#prev');
 const next = $('#next');
 const circles = $('.circle');
 
+//counter for button
+let button = 1
+
+//condition
+function ready() {
+    $('#next').on('click', () => {
+        button++;
+        console.log(button);
+    });
+}
